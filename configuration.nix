48fdb433747b6f7eed2fdb99193426657f4da3ec
wagar9386 <arnau.goti@gmail.com>
2026-06-15 11:42:48 +0200

    Bluetooth

--- configuration.nix
+++ configuration.nix
@@ -54,6 +54,8 @@
     rofi
     font-awesome
     discord
+    bluez
+    blueman
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -61,7 +63,11 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  {
+    hardware.bluetooth.enable = true;
 
+    services.blueman.enable = true;
+  }
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
   # console = {
