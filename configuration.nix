1f8e83883de4cc1291a94da6ee0039366d77956f
wagar9386 <arnau.goti@gmail.com>
2026-06-16 20:15:27 +0200

    unfree= true

--- configuration.nix
+++ configuration.nix
@@ -91,6 +91,7 @@
 
   services.blueman.enable = true;
   
+  nixpkgs.config.allowUnfree = true;  
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
   # console = {
