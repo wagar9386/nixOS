ca4c20a0e1d9ba3030f76e974458c465f489eff7
wagar9386 <arnau.goti@gmail.com>
2026-06-16 20:01:21 +0200

    nix flake update

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -85,7 +85,7 @@ hl.config({
         rounding_power = 2,
 
         active_opacity   = 1.0,
-        inactive_opacity = 0.88,
+        inactive_opacity = 0.93,
 
         shadow = {
             enabled      = true,
