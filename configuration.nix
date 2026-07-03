afb9f3916e9e057390790f5a1a95ffc4700742a6
wagar9386 <arnau.goti@gmail.com>
2026-07-04 00:03:31 +0200

    auto-optimise-store(deduplicate store)

--- configuration.nix
+++ configuration.nix
@@ -88,7 +88,7 @@
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
+  nix.settings.auto-optimise-store = true;
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
