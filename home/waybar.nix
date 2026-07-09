cad7e7769eee339fd6d0e82b60bc5894032d26bc
wagar9386 <arnau.goti@gmail.com>
2026-07-09 13:11:53 +0200

    removed manual systemd block

--- home/waybar.nix
+++ home/waybar.nix
@@ -36,20 +36,4 @@ in
 
   home.packages = [ pkgs.playerctl ];
 
-  systemd.user.services.waybar = {
-    Unit = {
-      Description = "Waybar status bar";
-      PartOf = [ "graphical-session.target" ];
-      After = [ "graphical-session.target" ];
-    };
-    Service = {
-      Type = "simple";
-      ExecStart = "${pkgs.waybar}/bin/waybar";
-      Restart = "on-failure";
-      RestartSec = 1;
-    };
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
-    };
-  };
 }
