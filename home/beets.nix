72ac9b76d2dee1d9072ba56d8571e86961548ea0
wagar9386 <arnau.goti@gmail.com>
2026-07-20 13:30:23 +0200

    no bullshit

--- home/beets.nix
+++ home/beets.nix
@@ -1,11 +1,5 @@
 { config, pkgs, ... }:
 
 {
-    home.packages = [
-        (pkgs.beets.override {
-            extras = true;
-        })
-    ];
-
     xdg.configFile."beets/config.yaml".source = ../config/beets/config.yaml;
 }
